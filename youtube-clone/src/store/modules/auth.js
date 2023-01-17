ff525d9c25ba316e5160a2df7f444fda8f519e02
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-17 16:05:26 +0700

    authentication on client

--- youtube-clone/src/store/modules/auth.js
+++ youtube-clone/src/store/modules/auth.js
@@ -1,24 +1,89 @@
-import Api from '@/services/Api'
+import AuthenticationService from "../../services/AuthenticationService";
+
+const state = {
+  url: process.env.VUE_APP_URL,
+  token: localStorage.getItem("token") || null,
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  isUserLoggedIn: localStorage.getItem("token") || false,
+};
+
+const getters = {
+  getUrl: (state) => {
+    return state.url;
+  },
+  isAuthenticated: (state) => {
+    return state.isUserLoggedIn;
+  },
+  getToken: (state) => {
+    return state.token;
+  },
+  currentUser: (state) => {
+    return state.user;
+  },
+};
+
+const mutations = {
+  SET_TOKEN(state, token) {
+    state.token = token;
+    if (token) {
+      state.isUserLoggedIn = true;
+    } else {
+      state.isUserLoggedIn = false;
+    }
+  },
+  SET_USER_DATA(state, data) {
+    state.user = data;
+  },
+  CLEAR_AUTH(state) {
+    state.token = null;
+    state.user = null;
+    state.isUserLoggedIn = false;
+  },
+};
+
+const actions = {
+  signUp({ commit }, credentials) {
+    return new Promise((resolve, reject) => {
+      AuthenticationService.signUp(credentials)
+        .then(({ data }) => {
+          commit("SET_TOKEN", data.token);
+          localStorage.setItem("token", data.token);
+          resolve(data);
+        })
+        .catch((error) => reject(error));
+    });
+  },
+  signIn({ commit }, credentials) {
+    return new Promise((resolve, reject) => {
+      AuthenticationService.signIn(credentials)
+        .then(({ data }) => {
+          localStorage.setItem("token", data.token);
+          commit("SET_TOKEN", data.token);
+        })
+        .catch((error) => reject(error));
+    });
+  },
+  getUser({ commit }, token) {
+    return new Promise((resolve, reject) => {
+      AuthenticationService.me(token)
+        .then(({ data }) => {
+          localStorage.setItem("user", JSON.stringify(data.data));
+          commit("SET_USER_DATA", data.data);
+          resolve(data);
+        })
+        .catch((error) => reject(error));
+    });
+  },
+  signOut({ commit }) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    commit("CLEAR_AUTH");
+  },
+};
 
 export default {
-  signIn(credentials) {
-    return Api().post('auth/login', credentials)
-  },
-  signUp(data) {
-    return Api().post('auth/register', data)
-  },
-  updateUserDetails(data) {
-    return Api().put('auth/updatedetails', data)
-  },
-  uploadUserAvatar(data) {
-    return Api().put('auth/avatar', data)
-  },
-  updatePassword(data) {
-    return Api().put('auth/updatepassword', data)
-  },
-  me(token) {
-    return Api().post('auth/me', {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-  }
-}
+  state,
+  getters,
+  mutations,
+  actions,
+};
